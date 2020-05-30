5d7aa463be35657fd4f595ec376c8e0c623bae69
евгений <79500420830@yandex.ru>
2020-05-30 14:01:26 +0300

    modify out to file

--- main.cpp
+++ main.cpp
@@ -2,38 +2,48 @@
 #include <fstream>
 
 using namespace std;
+
+class Redaktor {
+public:
+    static int find(char del, string line, char letter) {
+        int counter = 0;//for check a count of sumbols
+        int result = 0;
+        if (line[0] == letter)
+            result++;
+        for (int i = 0; i < line.size(); i++) {
+            if (line[i] == del) {
+              counter++;
+                while (line[i + counter] == del) {
+                        counter++;
+                }
+                if(line[i + counter] == letter)
+                {
+                    result++;
+                    i+=counter;
+                }
+                    counter = 0;
+            }
+        }
+        return result;
+    }
+};
+
 int main() {
     string nameOfFile;
-    char letter;
-    int counter = 0;
-    cout << "hello, this program can check how many word in this sentence have on first position have a certain letter\n"
-            "if you want start you need write name of file, and on next line write letter";
+    char letter = ' ';
+    cout
+            << "hello, this program can check how many word in this sentence have on first position have a certain letter\n"
+               "if you want start you need write name of file, and on next line write letter\n";
     cin >> nameOfFile;
     cin >> letter;
     string line;
     ifstream in;
     in.open(nameOfFile);
-    if (in.is_open())
-    {
+    if (in.is_open()) {
         getline(in, line);
-        string delim = " ";
-        size_t pos = 0;
-        size_t base_str_size = line.size();
-        size_t delim_size = delim.size();
-        string temp;
-        int position = -1;
-        if (pos >= 0)
-            while (pos < base_str_size) {
-            temp = temp.assign(line, pos, line.find(delim, pos) - pos);
-                position = temp.find_first_of(letter);
-                if(position == 0) counter++;
-                pos += temp.size() + delim_size;
-                position = -1;
-        }
+        in.close();
+        ofstream outf(nameOfFile, ios::app);
+        outf << "\n" << "for letter " << letter << ", count = " << Redaktor::find(' ', line, letter);
+        return 0;
     }
-    in.close();
-    ofstream outf(nameOfFile,ios::app);
-    outf << "\n" <<"for letter " << letter << ", count = " << counter;
-    return 0;
 }
-
